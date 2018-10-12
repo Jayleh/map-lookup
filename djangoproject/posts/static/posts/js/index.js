aea76700aba89e9ae95273b5ef419f16ad54671e
Jayleh <jaylehyun@gmail.com>
2018-10-12 08:22:06 -0700

    todo: fix sidenav bug

--- djangoproject/posts/static/posts/js/index.js
+++ djangoproject/posts/static/posts/js/index.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let $sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init($sidenav);
+    let $sidenav = document.querySelector('.sidenav'),
+        options = {
+            closeOnClick: false
+        }
+    // M.Sidenav.init($sidenav, options);
+
+    let instance = M.Sidenav.getInstance($sidenav);
+
+    console.log(instance);
+
+    // Sidenav open and close buttons
+    let openSidenavBtn = document.querySelector('.open-sidenav'),
+        closeSidenavBtn = document.querySelector('.close-sidenav');
+
+    openSidenavBtn.addEventListener('click', function () {
+        instance.open();
+    });
+
+    closeSidenavBtn.addEventListener('click', function () {
+        instance.close();
+    });
 });
