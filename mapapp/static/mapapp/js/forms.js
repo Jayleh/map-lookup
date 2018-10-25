a786f50503bcf7b1b0376959f84193993d1a1757
Jayleh <jaylehyun@gmail.com>
2018-10-25 16:26:38 -0700

    starting import export functionality

--- mapapp/static/mapapp/js/forms.js
+++ mapapp/static/mapapp/js/forms.js
@@ -2,4 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     // Initialize select field
     let $select = document.querySelector('select');
     M.FormSelect.init($select);
+
+    // Initialize select field
+    let $modal = document.querySelector('.modal');
+    M.Modal.init($modal);
 });
